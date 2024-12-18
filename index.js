eff1f5a9d26adece4c9cdea4b422dd2d6ed6d42b
Shubin123 <wangshubin130@gmail.com>
2024-12-17 17:11:17 -0800

    Update index.js

--- index.js
+++ index.js
@@ -228,12 +228,55 @@ function animateChart() {
     currentIndex++;
 
     // Call the next frame of the animation loop
-    requestAnimationFrame(animateChart);
+    // requestAnimationFrame(animateChart);
   }
 }
 
+
+
 // Start the animation loop (start the data scroll)
 animateChart();
+
+
+var stop = false;
+var frameCount = 0;
+// var $results = $("#results");
+var fps, fpsInterval, startTime, now, then, elapsed;
+
+function startAnimating(fps) {
+    fpsInterval = 1000 / fps;
+    then = Date.now();
+    startTime = then;
+    animate();
+}
+
+function animate() {
+
+    // request another frame
+
+    requestAnimationFrame(animate);
+
+    // calc elapsed time since last loop
+
+    now = Date.now();
+    elapsed = now - then;
+
+    // if enough time has elapsed, draw the next frame
+
+    if (elapsed > fpsInterval) {
+
+        // Get ready for next frame by setting then=now, but also adjust for your
+        // specified fpsInterval not being a multiple of RAF's interval (16.7ms)
+        then = now - (elapsed % fpsInterval);
+
+        // Put your drawing code here
+        animateChart();
+
+    }
+}
+
+startAnimating(10)
+
     </script>
   </body>
   </html>
